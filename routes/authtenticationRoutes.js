bfb1f6eecc7d3e3fbb99334e0b86ce6261ab1e1a
asherDn-Official <developers@asherdn.com>
2024-11-15 15:23:20 +0530

    Update authtenticationRoutes.js

--- routes/authtenticationRoutes.js
+++ routes/authtenticationRoutes.js
@@ -11,19 +11,21 @@ const rateLimit = require("express-rate-limit");
 
 // Define rate limiter for login route
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 login requests per `windowMs`
-  message: {
+  windowMs: 15 * 60 * 1000, 
+  max: 5, 
     message:
       "Too many login attempts from this IP, please try again after 15 minutes",
   },
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  standardHeaders: true, 
+  legacyHeaders: false,
 });
-
+// Resister a User
 router.post("/register", register);
-router.post("/login", login);
+// Login to a User Account
+router.post("/login",loginLimiter, login);
+// Logout a User
 router.post("/logout", logout);
+// access a profie data (protected) login must
 router.get("/profile", protect, getProfile);
 
 module.exports = router;
